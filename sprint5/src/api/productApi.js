758855cb48c92ea93124e2f27351b4ded8b9db1a
jellybrian12 <rkdwotjd1017@gmail.com>
2026-09-23 09:32:56 +0900

    sprint5 url render로 변경

--- sprint5/src/api/productApi.js
+++ sprint5/src/api/productApi.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://panda-market-jnwj.onrender.com';
 
 export const getProducts = async ({
   offset = 0,
